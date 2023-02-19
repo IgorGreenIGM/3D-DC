74345ea357dd8078edf9645e119933c858569bf0
IgorGreenIGM <igormogou86@gmail.com>
2023-02-19 08:25:35 +0100

    added file containing unary test updated the scrapper in python updated thr ploter

---
+++ .VSCodeCounter/2023-02-17_10-07-37/diff.md
@@ -0,0 +1,24 @@
+# Diff Summary
+
+Date : 2023-02-17 10:07:37
+
+Directory c:\\Users\\pc\\Desktop\\C++\\DCompress
+
+Total : 4 files,  115 codes, 4 comments, 40 blanks, all 159 lines
+
+[Summary](results.md) / [Details](details.md) / Diff Summary / [Diff Details](diff-details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| C++ | 4 | 115 | 4 | 40 | 159 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 4 | 115 | 4 | 40 | 159 |
+| src | 4 | 115 | 4 | 40 | 159 |
+| src (Files) | 1 | 90 | 4 | 36 | 130 |
+| src\\DCompress | 3 | 25 | 0 | 4 | 29 |
+
+[Summary](results.md) / [Details](details.md) / Diff Summary / [Diff Details](diff-details.md)
---
+++ scrapper.py
@@ -0,0 +1,47 @@
+#coding:utf-8
+
+import os
+import pathlib
+import threading
+import time
+
+# argv[1] = file path + name
+# argv[2] = matrix size
+# argv[3] = buffer size
+# argv[4] = queue size
+
+queue_size = 3000
+buffer_size = 50000
+file_path = "img.png"
+
+sizes = []
+
+# creating initial file archive 
+os.system(f"rar a init -m5 {file_path} > tmp.txt")
+init_size = pathlib.Path("./init.rar").stat().st_size
+print("Init file compress Done-------------------------------")
+
+# we start by changing the matrix size
+with open("matrix_2D.txt", mode="w") as _2D, open("matrix_3D.txt", mode="w") as _3D, open("matrix_2D_3D.txt", mode="w") as _2D_3D:
+    for matrix_size in range(1, 1000):
+        print(f"process status : {matrix_size} / 1000")
+
+        print("\n\nProcessing 2D : ---------------------------------------------------------------------------------------\n")
+        os.system(f"output.exe img.png {matrix_size} {queue_size} 1 0")
+        os.system(f"rar a fltred_2D_{matrix_size} -m5 filtered__2D_{file_path} > tmp.txt")
+        _2D.write(f"matrix size : {matrix_size} : init size : {init_size} filtred_size = {pathlib.Path(f'fltred_2D_{matrix_size}.rar').stat().st_size}\n")
+        print("2D Processing finished-----------------------------------------------------------------------------------------")
+
+        print("\n\nProcessing 3D : ---------------------------------------------------------------------------------------\n")
+        os.system(f"output.exe img.png {matrix_size} {queue_size} 0 1")
+        os.system(f"rar a fltred_3D_{matrix_size} -m5 filtered__3D_{file_path} > tmp.txt")
+        _3D.write(f"matrix size : {matrix_size} : init size : {init_size} filtred_size = {pathlib.Path(f'fltred_3D_{matrix_size}.rar').stat().st_size}\n")
+        print("3D Processing finished-----------------------------------------------------------------------------------------")
+
+        print("\n\nProcessing 2D_3D : ---------------------------------------------------------------------------------------\n")
+        os.system(f"output.exe img.png {matrix_size} {queue_size} 1 1")
+        os.system(f"rar a fltred_2D_3D_{matrix_size} -m5 filtered__2D_3D_{file_path} > tmp.txt")
+        _2D_3D.write(f"matrix size : {matrix_size} : init size : {init_size} filtred_size = {pathlib.Path(f'fltred_2D_3D_{matrix_size}.rar').stat().st_size}\n")
+        print("2D_3D Processing finished-----------------------------------------------------------------------------------------\n")
+
+        os.system("cls")
