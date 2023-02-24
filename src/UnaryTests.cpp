0e0b01c9c9f90dad939067edf758772d69d0ff9a
IgorGreenIGM <igormogou86@gmail.com>
2023-02-24 21:24:29 +0100

    modified unray test according to DCQueue modifications

--- src/UnaryTests.cpp
+++ src/UnaryTests.cpp
@@ -13,8 +13,8 @@ void test_if_build_method_works_to_get_all_datas(const std::string &file_path, i
     std::cout << "number of matrix build : " << build_nb << "\n";
     std::cout << "number of matrix in the queue : " << queue.size() << "\n";
 
-    const auto &all = queue.get_linear(false, false); std::cout << "get linear all datas okk--\n";
-
+    const auto &all = queue.linear_parse(); std::cout << "get linear all datas okk--\n";
+    std::cout << "size of datas got from queue : " << all.size() << "\n";
     std::ofstream out("out_" + file_path, std::ios::binary);
     for (int i = 0; i < all.size(); ++i)
         out << std::noskipws << all[i];
@@ -37,24 +37,24 @@ void test_if_filter_works_to_generate_filter_dictionnary(const std::string &file
     std::cout << "number of matrix build : " << build_nb << "\n";
     std::cout << "number of matrix in the queue : " << queue.size() << "\n";
 
-    const auto &fltrs = queue.filter(true, true);
-    std::cout << "size of the filters dictionnary output : " << fltrs.size() << "--\n";
+    // const auto &fltrs = queue.filter(true, true);
+    // std::cout << "size of the filters dictionnary output : " << fltrs.size() << "--\n";
     std::cout << "must be filters dictionnary output size : " << (build_nb * mat_size) + (mat_size * mat_size) << "--\n";
 
-    if (fltrs.size() != (build_nb * mat_size) + (mat_size * mat_size))
-        throw std::runtime_error("error when in filters size");
+    // if (fltrs.size() != (build_nb * mat_size) + (mat_size * mat_size))
+        // throw std::runtime_error("error when in filters size");
     
     std::cout << "finding the first 3D filter position, must be at : " << (build_nb * mat_size) << "(after 2D filter)\n";
-    auto found = std::find_if(fltrs.begin(), fltrs.end(), [](const uint8_t &v){return (int)v >= 5;});
-    if (std::distance(fltrs.begin(), found) != (build_nb * mat_size))
-        throw std::runtime_error("error in 3D filters positions");
-    else
-        std::cout << "3D filter position starts okk, found at position : " << std::distance(fltrs.begin(), found) << "--\n";
+    // auto found = std::find_if(fltrs.begin(), fltrs.end(), [](const uint8_t &v){return (int)v >= 5;});
+    // if (std::distance(fltrs.begin(), found) != (build_nb * mat_size))
+        // throw std::runtime_error("error in 3D filters positions");
+    // else
+        // std::cout << "3D filter position starts okk, found at position : " << std::distance(fltrs.begin(), found) << "--\n";
 
     std::ofstream out("out_filter_" + file_path, std::ios::binary);
-    for (const auto &fl : fltrs)
-        out << std::noskipws << fl;
-    std::cout << "generating filter dictionnary output okk--\n";
+    // for (const auto &fl : fltrs)
+        // out << std::noskipws << fl;
+    // std::cout << "generating filter dictionnary output okk--\n";
 }
 
 /*PASSED, fixed and rewrited multithreading in filter method*/
@@ -66,16 +66,16 @@ void test_if_filter_then_unfilter_works(const std::string &file_path, int mat_si
     DCQueue queue(buffer, queue_size, mat_size); std::cout << "queue init okk--\n";
 
     auto build_nb = queue.build(del1, del2); std::cout << "queue build okk--\n";
-    
-    auto fltrs(queue.filter(true, true));
+
+    queue._3D_filter();   
+    queue._3D_filter();   
     std::cout << "filter queue okk--\n";
 
     std::cout << "matrix number inside the queue : " << build_nb << "--\n";
-    std::cout << "filter size : " << fltrs.size() << "\n";
-    queue.unfilter(fltrs, true, true);
+    queue.unfilter();
     std::cout << "unfilter queue okk--\n";
 
-    const auto &datas = queue.get_linear(false, false);
+    const auto &datas = queue.linear_parse();
     std::cout << "retrieve datas okk--\n";
 
     std::cout << "all size : " << datas.size() << "\n";
@@ -97,9 +97,9 @@ void filter_and_save_filtered_file(const std::string &file_path, bool _2D, bool 
 
     auto build_nb = queue.build(del1, del2); std::cout << "queue build okk--\n";
     
-    queue.filter(_2D, _3D); std::cout << "filter queue okk..........\n";
+    // queue.filter(_2D, _3D); std::cout << "filter queue okk..........\n";
 
-    auto datas(queue.get_linear(true, false)); std::cout << "retrieve datas okk..........\n";
+    auto datas(queue.linear_parse()); std::cout << "retrieve datas okk..........\n";
 
     auto add = (_2D and _3D) ? "_2D_3D_"s : _3D ? "_3D_"s : "_2D_"s; 
     std::ofstream out("filtered_" + add + file_path, std::ios::binary);
@@ -129,11 +129,11 @@ void test_if_unfilter_works_to_read_and_unfilters_datas(const std::string &file_
     
     std::cout << "reading filters dictionnary from filter dictionnary okk--\n";
     std::cout << "Starting unfilter\n";
-    queue.unfilter(fltrs_in, true, true);
+    // queue.unfilter(fltrs_in, true, true);
     std::cout << "unfilter okk--\n";
 
     std::cout << "retrieving unfiltred datas from file : ...\n";
-    const auto &all_datas = queue.get_linear(true, false);
+    const auto &all_datas = queue.linear_parse();
     std::cout << "retrieving unfiltred datas from file okk--\n";
 
     std::cout << "writing unfiltred datas into file...\n";
@@ -156,9 +156,9 @@ void _2D_get(const std::string &file_path, int mat_size, int queue_size, int buf
 
     auto build_nb = queue.build(del1, del2); std::cout << "queue build okk--\n";
     
-    queue.filter(true, false); std::cout << "2D filter queue okk..........\n";
+    // queue.filter(true, false); std::cout << "2D filter queue okk..........\n";
 
-    auto datas(queue.get_all_z()); std::cout << "retrieve datas in 2D okk..........\n";
+    auto datas(queue.z_parse()); std::cout << "retrieve datas in 2D okk..........\n";
 
     auto add = "_2D_"s;
  
@@ -178,9 +178,9 @@ void _3D_get(const std::string &file_path, int mat_size, int queue_size, int buf
 
     auto build_nb = queue.build(del1, del2); std::cout << "queue build okk--\n";
     
-    queue.filter(false, true); std::cout << "3D filter queue okk..........\n";
+    // queue.filter(false, true); std::cout << "3D filter queue okk..........\n";
 
-    auto datas(queue.get_all_z()); std::cout << "retrieve datas in 3D okk..........\n";
+    auto datas(queue.z_parse()); std::cout << "retrieve datas in 3D okk..........\n";
 
     auto add = "_3D_"s;
  
@@ -199,7 +199,7 @@ double progressive_entropy(const std::string &file_name, int matrix_size, int bu
     while(true)
     {
         auto built_nb = queue.build(2, 2);
-        queue.filter(_2D, _3D);
+        // queue.filter(_2D, _3D);
         _entropy += queue.entropy();
         if (built_nb < queue_size)
             break;
